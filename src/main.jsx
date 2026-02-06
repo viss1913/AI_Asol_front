77596b5e9b61d0d0155a7385a6fc25b17d8c2b5c
viss1913 <vissarovav@gmail.com>
2026-02-06 14:17:05 +0300

    fix: восстановлен импорт App в main.jsx (причина белого экрана)

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { UserProvider } from './context/UserContext'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
